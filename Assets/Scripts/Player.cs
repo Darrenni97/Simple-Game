83996cb34108e19960bf7f8b64e378634a5f8fa4
Darrenni97 <darrenni_@hotmail.com>
2020-03-02 19:53:30 -0800

    move player with velocity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  [SerializeField]
+  private Rigidbody playerBody;
+  // Start is called before the first frame update
+  void Start()
+  {
+    playerBody = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    playerBody.velocity = new Vector3(2f, playerBody.velocity.y, 2f);
+  }
+}
