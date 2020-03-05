4f32b20cb485ebf7bc636574adf655861f332735
Darrenni97 <darrenni_@hotmail.com>
2020-03-05 15:47:48 -0800

    logic for sword to perform attack

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+  private float attackLength = 0.5f;
+
+  // Update is called once per frame
+  void Update () {
+    attackLength -= Time.deltaTime;
+    if (attackLength <= 0) {
+      gameObject.SetActive (false);
+      attackLength = 0.5f;
+    }
+  }
+}
