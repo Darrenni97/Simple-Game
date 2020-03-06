f1117390f983059a60108d95eb07eb469984ae5b
Darrenni97 <darrenni_@hotmail.com>
2020-03-05 16:25:32 -0800

    add panel to game object to remain disabled

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameMenu : MonoBehaviour {
+    private GameObject menu;
     // Start is called before the first frame update
     void Start () {
-
+        menu = transform.GetChild (0).gameObject;
     }
 
     // Update is called once per frame
     void Update () {
         if (Input.GetButtonDown ("Cancel")) {
-            gameObject.SetActive (!gameObject.activeSelf);
+            menu.SetActive (!menu.activeSelf);
         }
     }
 }
